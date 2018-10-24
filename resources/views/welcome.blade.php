8b89210b2bb1b56925f228310f089f9772dede5c
Ruben E. Rangel <rubenrangel@indigosnetwork.com>
2018-10-23 23:10:14 -0400

    Change message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel e Indigos enseñando Git
                 </div>
 
                 <div class="links">
